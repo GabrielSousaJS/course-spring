4c5512cf97ec3ed99a817ea0358d01892f3c0bb2
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-01 00:28:46 -0300

    Subtotal & Total methods

--- src/main/java/com/gabrielsousa/coursespring/entities/Order.java
+++ src/main/java/com/gabrielsousa/coursespring/entities/Order.java
@@ -92,6 +92,15 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        Double sum = 0.0;
+        for (OrderItem purchases : items) {
+            sum += purchases.getSubTotal();
+        }
+
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
