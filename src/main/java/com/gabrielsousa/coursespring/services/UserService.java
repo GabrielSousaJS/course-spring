1a3384d0b644d1dc2ea744bc572ed7a9347b1853
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-02 00:37:34 -0300

    Exception handling - findById

--- src/main/java/com/gabrielsousa/coursespring/services/UserService.java
+++ src/main/java/com/gabrielsousa/coursespring/services/UserService.java
@@ -2,6 +2,8 @@ package com.gabrielsousa.coursespring.services;
 
 import com.gabrielsousa.coursespring.entities.User;
 import com.gabrielsousa.coursespring.repositories.UserRepository;
+import com.gabrielsousa.coursespring.resources.exceptions.ResourceExceptionHandler;
+import com.gabrielsousa.coursespring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
